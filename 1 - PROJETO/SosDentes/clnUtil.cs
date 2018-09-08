03507643ea0e4ed29a738f294902851be533cc12
Adrian Gonçalves <adrianamazonas2015@gmail.com>
2018-09-08 17:45:04 -0300

    Removendo usings desnecessários

--- 1 - PROJETO/SosDentes/clnUtil.cs
+++ 1 - PROJETO/SosDentes/clnUtil.cs
@@ -1,13 +1,7 @@
 ﻿using SosDentes.BD;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Text.RegularExpressions;
-using static System.Convert;
 
 
 namespace SosDentes
